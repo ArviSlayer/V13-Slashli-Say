e2d7cc5ead9332d80a910c2ee838c245b5630919
ArviS <69751083+ArviSlayer@users.noreply.github.com>
2022-04-10 20:29:23 +0300

    Create say.js

---
+++ say.js
@@ -0,0 +1,35 @@
+"ArviS - V13 Slash Say"
+
+
+const { ApplicationCommandType, ApplicationCommandOptionType, ChatInputCommandInteraction, Client, EmbedBuilder} = require('discord.js');
+
+module.exports = {
+    type: ApplicationCommandType.ChatInput,
+    name: 'say',
+    description: 'Üye Sayacı',
+    options: [],
+    /**
+     * Run a chat input command interaction.
+     * @param {ChatInputCommandInteraction} interaction The interaction.
+     * @param {Client} client The client of the bot.
+     */
+    async run(interaction, client) {
+
+  const voiceChannels = interaction.guild.channels.cache.filter(c => c.type === "voice");
+  let count = 0;
+
+  for (const [id, voiceChannel] of voiceChannels)
+  count += voiceChannel.members.size;
+  const embed = new EmbedBuilder()
+  .setAuthor({ name: interaction.guild.name, iconURL: interaction.user.displayAvatarURL() })
+  .setDescription(
+    `**Sunucuda Toplam \`${interaction.guild.memberCount}\` Kişi Bulunmakta.**`)
+ 
+
+    .setThumbnail(interaction.guild.iconURL())
+    .setFooter({ text: client.user.username, iconURL: client.user.avatarURL() })
+    .setTimestamp();
+   
+    interaction.reply({ embeds: [embed] })
+  }
+};
